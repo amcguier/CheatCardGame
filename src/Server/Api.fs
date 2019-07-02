0b8a3b8c1177e03189b6914e5279b51bcdb6c761
Andrew McGuier <amcguier@gmail.com>
2019-07-02 00:56:59 -0400

    Setting up redis and creating game

--- src/Server/Api.fs
+++ src/Server/Api.fs
@@ -1,20 +1,61 @@
 module Api
 
+open System
 open System.Threading.Tasks
 open FSharp.Control.Tasks.V2
 open Giraffe
 open Saturn
 open StackExchange.Redis
 open Shared
+open Thoth.Json.Net
+open System.Collections.Generic
 
 let redis = ConnectionMultiplexer.Connect("localhost")
 let db = redis.GetDatabase()
+let inline (~~) (x : ^a) : ^b =
+  ((^a or ^b) : (static member op_Implicit : ^a -> ^b) x)
 let getInitCounter() : Task<Counter> = task { return { Value = 42 } }
+let gameKey = sprintf "game:%s"
+
+let storeRedis (key : string) value (timeSpan : TimeSpan Option) =
+  let ky : RedisKey = ~~key
+  let vl : RedisValue = ~~(Encode.Auto.toString (2, value))
+  if timeSpan.IsSome then
+    db.StringSet
+      (ky, vl,
+       Option.defaultWith (fun () -> TimeSpan.FromMinutes 0.0) timeSpan
+       |> Nullable)
+  else db.StringSet(ky, vl)
+
+let createGame (newGame : NewGame) =
+  let id = Guid.NewGuid().ToString()
+
+  let game =
+    { GameId = id
+      Owner = newGame.Username
+      Players = newGame.Players
+      PlayersConnected = 0 }
+  TimeSpan.FromMinutes 15.0
+  |> Some
+  |> storeRedis (gameKey id) game
+  |> ignore
+  game
 
 let gameController =
   controller {
     index (fun ctx -> "hello world" |> Controller.json ctx)
-    show (fun ctx id -> sprintf "Hello %s" id |> Controller.json ctx)
+    create (fun ctx ->
+      task {
+        let! (ng : NewGame) = Controller.getJson ctx
+        return ng
+               |> createGame
+               |> Controller.json ctx
+      })
+    show (fun ctx (id : string) ->
+      { Hand = []
+        PlayerCounts = []
+        DiscardCount = 0 }
+      |> Controller.json ctx)
   }
 
 let webApp =
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,4 +1,38 @@
 namespace Shared
 
+type Suit =
+  | Clubs
+  | Spades
+  | Hearts
+  | Diamonds
+
+type Value =
+  | Number of int
+  | Jack
+  | Queen
+  | King
+  | Ace
+
+type Card =
+  { Suit : Suit
+    Value : Value }
+
+type Hand = Card List
+
+type GameState =
+  { Hand : Hand
+    PlayerCounts : int list
+    DiscardCount : int }
+
+type NewGame =
+  { Username : string
+    Players : int }
+
+type Game =
+  { GameId : string
+    Owner : string
+    Players : int
+    PlayersConnected : int }
+
 type Counter =
-    { Value : int }
+  { Value : int }
